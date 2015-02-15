b1f9e117e66888a8cc255e3c1df28bfbe41b0b05
PablOCVi <a01630814@itesm.mx>
2015-02-15 15:46:41 -0600

    Mastery 192 primera version

---
+++ Mastery192.py
@@ -0,0 +1,15 @@
+print("Si el numero ingresado es par y mayor a 10, lo mltiplicare por 4")
+x=int(input("Ingresa un numero par, mayor a 10: "))
+while (x%2!=0)and(x>10):
+	if x%2!=0:
+		print("El numero "+str(x)+" es impar")
+	if x%2==0:
+		print("El numero "+str(x)+" es par")
+	if x<10:
+		print("El numero"+" "+str(x)+" "+"es menor a 10")
+	if x>10:
+		print("El numero"+" "+str(x)+" "+"es mayor a 10")
+	x=int(input("Ingresa un numero par, mayor a 10: "))
+print("El numero "+str(x)+" es par")
+print("El numero"+" "+str(x)+" "+"es mayor a 10")
+print("Al numero "+str(x)+" Lo multiplicare por 4 "+"= "+str(x*4))
